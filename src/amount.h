dfb19d47e9171667993cf16bd453ea3758bfb5cc
vanhalendlrband <vanhalendlrband@hotmail.com>
2024-11-27 11:05:45 -0800

    Update amount.h
    Trying to update the supply but not for zc

--- src/amount.h
+++ src/amount.h
@@ -29,7 +29,7 @@ extern const std::string MINOR_CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 21000000 * COIN;
+static const CAmount MAX_MONEY = 21000000 * COIN + 11462487 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /** The legacy default fee that was defined in ZIP 313. */
